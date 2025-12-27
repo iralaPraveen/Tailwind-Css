18a247dc9ffcf097e695818e147792d342b8779b
irala praveen <iralapraveenofficial@gmail.com>
2025-12-27 22:00:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Magic from "./components/Magic"
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl  bg-amber-300">hi friends how are you </h1>
+      <Magic />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Magic.jsx
@@ -0,0 +1,30 @@
+
+const Magic = () => {
+  return (
+    <div class="m-4 p-6 w-full min-h-screen bg-blue-300 rounded-lg flex justify-center items-center flex-col">
+      
+      <div class="gap-1 flex flex-col">
+        <h3>Tea</h3>
+        <h3>Coffee</h3>
+          <h3>Whisky</h3>
+          <h3>Scotch</h3>
+      </div>
+    
+      <button class="bg-gray-200 hover:bg-black hover:text-white transition p-2.5 border-b-emerald-950 border-green-400 px-3.5 rounded mt-4">
+  Click
+</button>
+<div class="max-w-sm p-6 bg-white rounded-xl shadow-lg">
+  <h2 class="text-xl font-semibold mb-2">Product</h2>
+  <p class="text-gray-600 mb-4">Clean UI using Tailwind</p>
+  <button class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+    Buy Now
+  </button>
+</div>
+
+      
+        
+    </div>
+  )
+}
+
+export default Magic
